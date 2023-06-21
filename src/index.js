9e8cc57f3cbad284599ba46776fdccaf6ca458ed
Danylo Kharkiv <donatelod12@gmail.com>
2023-06-22 00:05:31 +0300

    add home page in json & basename in index

--- src/index.js
+++ src/index.js
@@ -13,13 +13,9 @@ const root = createRoot(container);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/task-pro-frontend-team-project">
           <App />
         </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
