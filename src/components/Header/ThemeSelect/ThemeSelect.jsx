ff34c4573ae982e47179ebe6cb62158d4434f948
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-06-24 21:40:03 +0300

    Update ThemeSelect.jsx

--- src/components/Header/ThemeSelect/ThemeSelect.jsx
+++ src/components/Header/ThemeSelect/ThemeSelect.jsx
@@ -1,12 +1,1 @@
-export const ThemeSelect = () => {
-  return (
-    <div>
-      ThemeSelect
-      <ul>
-        {/* <li>Light</li>
-        <li>Dark</li>
-        <li>Violet</li> */}
-      </ul>
-    </div>
-  );
-};
+
