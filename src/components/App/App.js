f0958380a04e8172818dd48c9d4768ac029310a3
Danylo Kharkiv <donatelod12@gmail.com>
2023-06-25 16:25:47 +0300

    add lazy, change body style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Filter from '../Modals/Filters/Filters';
-import Modal from '../Modals/Modal';
-import { useEffect, useState } from 'react';
-import WelcomePage from '../../page/WelcomePage/WelcomePage';
-import AuthPage from '../../page/AuthPage/AuthPage';
-import HomePage from '../../page/HomePage/HomePage';
+import { Suspense, useEffect, useState } from 'react';
 import { PrivateRoute } from "../../route/PrivateRoute";
 import { RestrictedRoute } from "../../route/RestrictedRoute";
 import { useDispatch } from "react-redux";
 import { refreshUser } from "../../redux/auth/authOperation";
+import { lazy } from "react";
+
+const WelcomePage = lazy(() => import('../../page/WelcomePage/WelcomePage'));
+const AuthPage = lazy(() => import('../../page/AuthPage/AuthPage'));
+const HomePage = lazy(() => import('../../page/HomePage/HomePage'));
 
 
 function App() {
@@ -20,6 +20,7 @@ function App() {
   
   return (
     <>
+      <Suspense>
       <Routes>
         
         <Route path="/" element={
@@ -34,6 +35,7 @@ function App() {
         <Route path="*" element={<HomePage />} />
 
       </Routes>
+      </Suspense>
     </>
   );
 }
