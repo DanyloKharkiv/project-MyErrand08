040f2b6f5ab1209954c17b8e5e60af0741425f79
IschukM <dzubak@ukr.net>
2023-06-27 07:08:01 +0300

    add name and avatar

--- src/redux/auth/authSelector.js
+++ src/redux/auth/authSelector.js
@@ -7,3 +7,7 @@ export const selectIsRefreshing = state => state.auth.isRefreshing;
 export const selectError = state => state.auth.error;
 
 export const selectToken = state => state.auth.accessToken;
+
+ export const getAvatar = (state) => state.auth?.user?.avatarURL;
+
+ export const getName = (state) => state.auth?.user?.name;
