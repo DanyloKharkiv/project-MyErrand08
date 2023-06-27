68587d060a0965921221ad720f57d571ec33dbe6
Olena Vozniuk <110108199+Olena-Vozniuk@users.noreply.github.com>
2023-06-27 15:38:05 +0200

    add css-variables, theme component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import { UserInfo } from "./UserInfo/UserInfo";
 import { HeaderSection } from "./Header.Styled";
+import { ThemeSelect } from "./ThemeSelect/ThemeSelect";
 
 export const Header = () => {
   return (
     <HeaderSection>
+      <ThemeSelect/>
       <UserInfo />
     </HeaderSection>
   );
--- src/components/Header/ThemeSelect/ThemeSelect.jsx
+++ src/components/Header/ThemeSelect/ThemeSelect.jsx
@@ -1,1 +1,43 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setTheme } from "../../../redux/theme/themeSlice";
+import {Container, Title, Wrapper, List, Item} from "./ThemeSelect.Styled";
+import { IoIosArrowDown } from "react-icons/io";
 
+const themes = [
+  { name: "Light", value: "light" },
+  { name: "Dark", value: "dark" },
+  { name: "Violet", value: "violet" },
+];
+
+export const ThemeSelect = () => {
+  const dispatch = useDispatch();
+  const [isSelectOpen, setIsSelectOpen] = useState(false);
+  const [selectedTheme, setSelectedTheme] = useState("dark");
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", selectedTheme);
+  }, [selectedTheme]);
+
+  const handleThemeToggle = (theme) => {
+    dispatch(setTheme(theme));
+    setSelectedTheme(theme);
+    setIsSelectOpen(false);
+  };
+
+  return (
+    <Container isOpen={isSelectOpen}>
+      <Wrapper onClick={() => setIsSelectOpen(!isSelectOpen)}>
+        <Title>Theme</Title>
+        <IoIosArrowDown size={16} color="var(--themeTitle)"/>
+      </Wrapper>
+      <List isOpen={isSelectOpen}>
+        {themes.map(({ value, name }) => (
+          <Item key={value} onClick={() => handleThemeToggle(value)}>
+            {name}
+          </Item>
+        ))}
+      </List>
+    </Container>
+  );
+};
