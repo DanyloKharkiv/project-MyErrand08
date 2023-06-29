312d191448c565fb21f83764c0a132beb6ad3b29
Danylo Kharkiv <donatelod12@gmail.com>
2023-06-30 01:40:54 +0300

    add burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import { UserInfo } from "./UserInfo/UserInfo";
 import { HeaderSection } from "./Header.Styled";
 import { ThemeSelect } from "./ThemeSelect/ThemeSelect";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
-export const Header = () => {
+export const Header = ({openSideBar}) => {
   return (
     <HeaderSection>
+      <BurgerMenu openSideBar={openSideBar} />
       <ThemeSelect/>
       <UserInfo />
     </HeaderSection>
