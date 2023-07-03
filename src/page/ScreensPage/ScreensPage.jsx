f21957bdccbbaee857445acaec202ed39df68921
Danylo Kharkiv <donatelod12@gmail.com>
2023-07-04 01:14:49 +0300

    add bg

--- src/page/ScreensPage/ScreensPage.jsx
+++ src/page/ScreensPage/ScreensPage.jsx
@@ -6,28 +6,48 @@ import {
   FilterBtn,
 } from "./ScreensPage.Styled";
 import sprite from "../../images/sprite.svg";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import FilterModal from '../../components/Modals/Filters/FilterModal'
 import Columns from "../../components/Container/Column/Columns";
-import { selectActiveDeskId } from "../../redux/desk/deskSelectors";
+import { selectActiveDeskId, selectDesks } from "../../redux/desk/deskSelectors";
 import { useDispatch, useSelector } from "react-redux";
 import { getDeskById } from "../../redux/desk/deskOperations";
 import { DefaultScreen } from "../../components/DefaultScreen/DefaultScreen";
 import { useEffect } from "react";
+import desktop from "../../json/desktop.json" 
 
 export const ScreensPage = () => {
 
   const filterModalRef = useRef(null);
-
+  const [bgImg, setBgImg] = useState(null);
+  
   const openFilterModal = () => {
     if (filterModalRef.current) {
       filterModalRef.current.openModal()
     }
   }
-
+  const backgroundImg = useSelector(selectDesks);
+  
   const dispatch = useDispatch();
   const idDesk = useSelector(selectActiveDeskId);
 
+  useEffect(() => {
+    backgroundImg.map(elem => {
+    if (elem._id === idDesk) {
+      desktop.map(ele => {
+        if (ele.id === elem.background) {
+          setBgImg(ele.url) 
+        }
+      });
+    }
+    return
+  });
+  console.log(bgImg);
+  }, [idDesk])
+  
+  
+
+ 
     
     // const deskInfo = dispatch(getDeskById(idDesk));
     // console.log("Desk title", deskInfo);
@@ -56,6 +76,7 @@ export const ScreensPage = () => {
       <Columns idDesk={ idDesk } />
 
       <FilterModal ref={filterModalRef} />
+      <img src={bgImg} width="100px" height="100px" />
     </ScreensPageElement>
   );
 };
