1232cd7a10a6a9f539d8f07560542cdd4c9fde8a
Nazar Mosula <nmosula@gmail.com>
2023-07-06 08:12:27 +0300

    ScrollBar

--- src/components/Sidebar/Boards/BoardList.jsx
+++ src/components/Sidebar/Boards/BoardList.jsx
@@ -25,10 +25,16 @@ const DesksList = () => {
     }, [dispatch]);
   
   
-    const { isMobile } = useMatchMedia();
-    const hasScroll = !isMobile && visibleDesks.length > 2;
-    useScrollbar(desksWrapper, hasScroll);
+    // const { isMobile } = useMatchMedia();
+    // const hasScroll = !isMobile && visibleDesks.length > 2;
+    // useScrollbar(desksWrapper, hasScroll);
 
+    // const hasScroll = window.setTimeout(function () {
+    //   const viewex = visibleDesks > 2 ? true : false;
+      
+    // }, 500);
+    // useScrollbar(desksWrapper, hasScroll);
+  
     return (
         <BoardsList ref={desksWrapper}>
             {visibleDesks.length > 0 ? (
