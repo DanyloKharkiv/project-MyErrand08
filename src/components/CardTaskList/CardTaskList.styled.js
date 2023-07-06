a9963203a63fb9519f78de938731128ce4d51966
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-07-06 15:03:14 +0300

    Update CardTaskList.styled.js

--- src/components/CardTaskList/CardTaskList.styled.js
+++ src/components/CardTaskList/CardTaskList.styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const CardList = styled.div`
-  height: calc(100vh - 250px);
+  max-height: calc(100vh - 250px);
   /* border: 1px solid red; */
   overflow-y: auto;
   scrollbar-width: thin;
